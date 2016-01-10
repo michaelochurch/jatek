f8e635e8d6a9b8bb7cd3c9f398f900dc8f705c4d
Michael O. Church <michael.o.church@gmail.com>
2016-01-10 10:53:35 -0600

    Progress? Or should I fire myself from own project?

--- Jatek/Actor.hs
+++ Jatek/Actor.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE ExistentialQuantification, MultiParamTypeClasses #-}
+{-# LANGUAGE ExistentialQuantification, FlexibleInstances, KindSignatures, MultiParamTypeClasses #-}
 
 class Actor m a b self where
    act :: self -> a -> m b
 
 data SomeActor m a b = forall self . Actor m a b self => SomeActor self
+
+data PureActor (m :: * -> *) a b = PureActor (a -> b)
+
+instance (Monad m) => Actor m a b (PureActor m a b) where
+  act (PureActor f) a = return $ f a
