d4dd5c3c265fa059a4fa49af06d5eba4d9a05535
Michael O. Church <michael.o.church@gmail.com>
2016-01-13 19:43:44 -0600

    it turns out that I will need user state in InteractT

--- Jatek/Interact.hs
+++ Jatek/Interact.hs
@@ -21,6 +21,9 @@ import Jatek.Actor
 
 -- i : player ID type (often Int).
 
+-- u : user state. This shouldn't be folded into the monad m, because the
+-- function liftI won't "lens" the state of a subgame into a larger game.
+
 -- s : server message (e.g. a changing view from the game).
 -- c : client message (e.g. a player's action).
 
@@ -31,42 +34,51 @@ import Jatek.Actor
 -- View type is what part of the game each player can see. A simple InteractT
 -- use case might have the server sending views directly.
 
-data InteractT i c s m a =
+data InteractT i u c s m a =
   Terminal a |
-  Talk [(i, s)] ([(i, c)] -> InteractT i c s m a) |
-  M (m (InteractT i c s m a))
+  Talk [(i, s)] ([(i, c)] -> InteractT i u c s m a) |
+  Stateful (u -> (InteractT i u c s m a, u)) |
+  M (m (InteractT i u c s m a))
 
-instance (Monad m, Show a, Show s, Show i) => Show (InteractT i c s m a) where
+instance (Monad m, Show a, Show s, Show i) => Show (InteractT i u c s m a) where
   show (Terminal a) = "Terminal " ++ show a
   show (Talk msgs cont) = "Talk " ++ (show msgs) ++ " (λ)"
+  show (Stateful cont) = "Stateful (λ)"
   show (M _) = "M (..)"
 
-instance (Monad m) => Functor (InteractT i c s m) where
+instance (Monad m) => Functor (InteractT i u c s m) where
   fmap = liftM
 
-instance (Monad m) => Applicative (InteractT i c s m) where
+instance (Monad m) => Applicative (InteractT i u c s m) where
   pure = return
   (<*>) = ap
 
-instance (Monad m) => Monad (InteractT i c s m) where 
+instance (Monad m) => Monad (InteractT i u c s m) where 
   return = Terminal
   (Terminal a)     >>= k = k a
   (Talk msgs cont) >>= k = Talk msgs (\cs -> (cont cs) >>= k)
+  (Stateful cont)  >>= k = Stateful (\u -> first (flip (>>=) k) $ cont u)
   (M ma)           >>= k = M $ fmap (flip (>>=) k) ma
 
-instance MonadTrans (InteractT i c s) where
+instance (Monad m) => MonadState u (InteractT i u c s m) where
+  get    = Stateful (\u -> (Terminal u , u))
+  put u  = Stateful (\_ -> (Terminal (), u))
+
+instance MonadTrans (InteractT i u c s) where
   lift ma = M $ Terminal <$> ma
 
-instance MonadIO (InteractT i c s IO) where
+instance MonadIO (InteractT i u c s IO) where
   liftIO = lift
 
 -- WARNING: In stateful monads, e.g. m = IO, this *can* side-effect the System.
 -- That's by design. 
-runInteractT :: (Monad m, Ord i) => InteractT i c s m a -> System i m s c -> m a
-runInteractT intx system =
+runInteractT :: (Monad m, Ord i) => InteractT i u c s m a -> System i m s c -> u -> m (a, u)
+runInteractT intx system st =
    case intx of
-     Terminal a -> return a
+     Terminal a -> return (a, st)
      Talk send cont ->
-       sync system send >>= (\(intx1, sys1) -> runInteractT (cont intx1) sys1)
+       sync system send >>= (\(intx1, sys1) -> runInteractT (cont intx1) sys1 st)
+     Stateful cont ->
+       let (intx1, st1) = cont st in runInteractT intx1 system st1
      M cont ->
-       cont >>= (\intx1 -> runInteractT intx1 system)
+       cont >>= (\intx1 -> runInteractT intx1 system st)
